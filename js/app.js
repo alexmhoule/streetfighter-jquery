86926fc66d2b0bae192a3881444db7d0a5684044
alexmhoule <alexmhoule@gmail.com>
2015-04-10 00:24:09 -0500

    making progress with jquery

--- js/app.js
+++ js/app.js
@@ -6,5 +6,19 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		console.log('mousedown');
+		// play hadouken sound
+		//show hadouken and animate it to the right of the screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+	.mouseup(function() {
+		console.log('mouseup');
+		//ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
