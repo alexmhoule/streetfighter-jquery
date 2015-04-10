32e12c71c6f3cbd123911e68605e2b4d05bd0e08
alexmhoule <alexmhoule@gmail.com>
2015-04-10 00:45:24 -0500

    added sound and animation

--- js/app.js
+++ js/app.js
@@ -10,10 +10,18 @@ $(document).ready(function() {
 	.mousedown(function() {
 		console.log('mousedown');
 		// play hadouken sound
+		playHadouken();
 		//show hadouken and animate it to the right of the screen
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '625px');
+			}
+		);
 	})
 	.mouseup(function() {
 		console.log('mouseup');
@@ -22,3 +30,9 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
